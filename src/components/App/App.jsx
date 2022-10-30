94a5c0bb34561923547b30decc06168580a6f853
Oleksii <achili3f17@gmail.com>
2022-10-30 21:18:49 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,81 +1,70 @@
-import { useEffect, useState } from "react";
-import { nanoid } from "nanoid";
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 import ContactForm from '../ContactForm/ContactForm';
-import {ContactsList}  from "../ContactList/ContactList";
-import  {Filter}  from "../Filter/Filter";
-import { Wrap, TitleBook, ContactTitle, ContactWrap } from "./App.styled";
+import { ContactsList } from '../ContactList/ContactList';
+import { getLocalStorage, setLocalStorage } from 'constans/localStorage';
+import { Filter } from '../Filter/Filter';
+import { phoneBook } from 'constans/contactsBase';
+import { Wrap, TitleBook, ContactTitle, ContactWrap } from './App.styled';
 
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    getLocalStorage('contacts') ?? phoneBook
+  );
+  const [filter, setFilter] = useState('');
 
-
-export  const App = () =>  { 
-
-  const [contacts, setContacts] = useState(() =>(JSON.parse(localStorage.getItem('contacts'))) ??[
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ])
-  const [filter, setFilter] = useState('')
-
-
-const addToList = (name, number) => {
-
-for (const contact of contacts) {
-  if (contact.name.toLowerCase() === name.toLowerCase()) {
-    alert(`${name} is already in contacts`);
-    return;
-  }
-}
-const contact = {
-  id: nanoid(),
-  name,
-  number,
-};
-setContacts(prevState => [contact, ...prevState]);
-};
-
-const deleteContact = contactId => {
-    setContacts(prevState => (
-     prevState.filter(contact => contact.id !== contactId)
-    ));
+  const addToList = (name, number) => {
+    for (const contact of contacts) {
+      if (contact.name.toLowerCase() === name.toLowerCase()) {
+        alert(`${name} is already in contacts`);
+        return;
+      }
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts(prevState => [contact, ...prevState]);
   };
 
-const changeFilter = event => {
-    const {value} = event.currentTarget;
-    setFilter(value)
-  };
-
-const renderFilteredContact = () => {
-    const normalizedValue = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedValue)
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
     );
   };
 
-useEffect(() => {
-  localStorage.setItem('contacts', JSON.stringify(contacts))
-}, [contacts])
+  const changeFilter = event => {
+    const { value } = event.currentTarget;
+    setFilter(value);
+  };
 
+  const renderFilteredContact = () => {
+    const normalizedValue = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedValue)
+    );
+    return filteredContacts;
+  };
 
-const filteredContacts = renderFilteredContact();
-    
-    return (
-  <>
-    <Wrap>
-        <TitleBook>
-          Phonebook
-        </TitleBook>
+  useEffect(() => {
+    setLocalStorage('contacts', contacts);
+  }, [contacts]);
+
+  return (
+    <>
+      <Wrap>
+        <TitleBook>Phonebook</TitleBook>
         <ContactForm onSubmit={addToList} />
         <ContactTitle>Contacts</ContactTitle>
         <ContactWrap>
           <Filter value={filter} onChange={changeFilter} />
           <ContactsList
-            contacts={filteredContacts}
+            contacts={renderFilteredContact}
             onDelete={deleteContact}
           />
         </ContactWrap>
-    </Wrap>
-</>
-    )
-  }
+      </Wrap>
+    </>
+  );
+};
